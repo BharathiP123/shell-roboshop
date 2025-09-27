627215a3afdf8ff8fbc0c4e1740350ef3974ac11
bharathip123 <bharathip0528@gmail.com>
2025-09-27 13:37:14 -0600

    updated the catalogue file

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -11,6 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 MONGODBHOST=mongodb.bpotla.com
 mkdir -p $LOGS_FOLDER
+Script_dir=$PWD
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
