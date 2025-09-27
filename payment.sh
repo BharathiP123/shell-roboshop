bbaa17011f0264a5d396b6fd4c3f2999664147f0
bharathip123 <bharathip0528@gmail.com>
2025-09-27 16:04:30 -0600

    updated the all the files

--- payment.sh
+++ payment.sh
@@ -9,8 +9,9 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws86s.fun
+MONGODB_HOST=mongodb.bpotla.com
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+MYSQL_HOST=mysql.bpotla.com
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
@@ -29,13 +30,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-##### NodeJS ####
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling NodeJS"
-dnf module enable nodejs:20 -y  &>>$LOG_FILE
-VALIDATE $? "Enabling NodeJS 20"
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing NodeJS"
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
@@ -48,8 +43,8 @@ fi
 mkdir -p /app
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading cart application"
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading payment application"
 
 cd /app 
 VALIDATE $? "Changing to app directory"
@@ -57,18 +52,13 @@ VALIDATE $? "Changing to app directory"
 rm -rf /app/*
 VALIDATE $? "Removing existing code"
 
-unzip /tmp/cart.zip &>>$LOG_FILE
-VALIDATE $? "unzip cart"
+unzip /tmp/payment.zip &>>$LOG_FILE
+VALIDATE $? "unzip payment"
 
-npm install &>>$LOG_FILE
-VALIDATE $? "Install dependencies"
+pip3 install -r requirements.txt &>>$LOG_FILE
 
-cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
-VALIDATE $? "Copy systemctl service"
-
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
-systemctl enable cart &>>$LOG_FILE
-VALIDATE $? "Enable cart"
+systemctl enable payment  &>>$LOG_FILE
 
-systemctl restart cart
-VALIDATE $? "Restarted cart"
+systemctl restart payment
